470c704de9f13fb34f2fc206cff15d4035f15ed1
darkJay14 <jay14nath@gmail.com>
2017-04-18 10:17:57 +0530

    Add files via upload

---
+++ JPP/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace JPP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ShuffleButton = new System.Windows.Forms.Button();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.StretchButton = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 110);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Buttons_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(110, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 110);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Buttons_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(220, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 110);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Buttons_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Location = new System.Drawing.Point(330, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 110);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Buttons_Click);
+            // 
+            // button5
+            // 
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.Location = new System.Drawing.Point(0, 110);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(110, 110);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Buttons_Click);
+            // 
+            // button6
+            // 
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button6.Location = new System.Drawing.Point(110, 110);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(110, 110);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Buttons_Click);
+            // 
+            // button7
+            // 
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button7.Location = new System.Drawing.Point(220, 110);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(110, 110);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Buttons_Click);
+            // 
+            // button8
+            // 
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button8.Location = new System.Drawing.Point(330, 110);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(110, 110);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Buttons_Click);
+            // 
+            // button9
+            // 
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button9.Location = new System.Drawing.Point(0, 220);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(110, 110);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Buttons_Click);
+            // 
+            // button10
+            // 
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button10.Location = new System.Drawing.Point(110, 220);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(110, 110);
+            this.button10.TabIndex = 9;
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.Buttons_Click);
+            // 
+            // button11
+            // 
+            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button11.Location = new System.Drawing.Point(220, 220);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(110, 110);
+            this.button11.TabIndex = 10;
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.Buttons_Click);
+            // 
+            // button12
+            // 
+            this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button12.Location = new System.Drawing.Point(330, 220);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(110, 110);
+            this.button12.TabIndex = 11;
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.Buttons_Click);
+            // 
+            // button13
+            // 
+            this.button13.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button13.Location = new System.Drawing.Point(0, 330);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(110, 110);
+            this.button13.TabIndex = 12;
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.Buttons_Click);
+            // 
+            // button14
+            // 
+            this.button14.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button14.Location = new System.Drawing.Point(110, 330);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(110, 110);
+            this.button14.TabIndex = 13;
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.Buttons_Click);
+            // 
+            // button15
+            // 
+            this.button15.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button15.Location = new System.Drawing.Point(220, 330);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(110, 110);
+            this.button15.TabIndex = 14;
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.Buttons_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button15);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button14);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button13);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button12);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button11);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button10);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(440, 440);
+            this.panel1.TabIndex = 15;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(497, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(440, 440);
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // ShuffleButton
+            // 
+            this.ShuffleButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ShuffleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ShuffleButton.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.ShuffleButton.Location = new System.Drawing.Point(12, 477);
+            this.ShuffleButton.Name = "ShuffleButton";
+            this.ShuffleButton.Size = new System.Drawing.Size(57, 41);
+            this.ShuffleButton.TabIndex = 17;
+            this.ShuffleButton.Text = "Shuffle";
+            this.ShuffleButton.UseVisualStyleBackColor = false;
+            this.ShuffleButton.Click += new System.EventHandler(this.ShuffleButton_Click);
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.SearchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchButton.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.SearchButton.Location = new System.Drawing.Point(395, 477);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(57, 41);
+            this.SearchButton.TabIndex = 18;
+            this.SearchButton.Text = "New";
+            this.SearchButton.UseVisualStyleBackColor = false;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // StretchButton
+            // 
+            this.StretchButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.StretchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StretchButton.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.StretchButton.Location = new System.Drawing.Point(458, 214);
+            this.StretchButton.Name = "StretchButton";
+            this.StretchButton.Size = new System.Drawing.Size(33, 41);
+            this.StretchButton.TabIndex = 19;
+            this.StretchButton.Text = ">>>";
+            this.StretchButton.UseVisualStyleBackColor = false;
+            this.StretchButton.Click += new System.EventHandler(this.StretchButton_Click);
+            // 
+            // button16
+            // 
+            this.button16.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button16.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button16.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.button16.Location = new System.Drawing.Point(195, 486);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(75, 23);
+            this.button16.TabIndex = 20;
+            this.button16.Text = "ABOUT";
+            this.button16.UseVisualStyleBackColor = false;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(497, 528);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.StretchButton);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.ShuffleButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jay\'s Picture Puzzle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button ShuffleButton;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Button StretchButton;
+        private System.Windows.Forms.Button button16;
+    }
+}
+
---
+++ JPP/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JPP
+{
+    public partial class Form1 : Form
+    {
+
+        Point EmptyPoint;
+        Image ToBeResize;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ToBeResize = pictureBox1.Image; //Image.FromFile(Application.StartupPath + "\\pic.jpg");    BY changing it, I let the exe file picture independent. by jayanta
+            pictureBox1.BackgroundImage = ToBeResize;
+            EmptyPoint.X = 330;
+            EmptyPoint.Y = 330;
+            AddImagesToButton(ReturnCroppedList(Resize(ToBeResize), 110, 110));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Buttons_Click(object sender, EventArgs e)
+        {
+            MoveButton((Button)sender);
+        }
+
+        private void MoveButton(Button button)
+        {
+            int x = EmptyPoint.X - button.Location.X;
+            int px = x < 0 ? -x : x;
+
+            int y = EmptyPoint.Y - button.Location.Y;
+            int py = y < 0 ? -y : y;
+
+            //moving right & left
+            if (button.Location.Y.Equals(EmptyPoint.Y) && px.Equals(button.Size.Width))
+            {
+                button.Location = new Point(button.Location.X + x, button.Location.Y);
+                EmptyPoint.X -= x;
+            }
+
+            //moving top & buttom
+            if (button.Location.X.Equals(EmptyPoint.X) && py.Equals(button.Size.Width))
+            {
+                button.Location = new Point(button.Location.X, button.Location.Y + y);
+                EmptyPoint.Y -= y;
+            }
+        }
+
+        public void AddImagesToButton(ArrayList pieces)
+        {
+            int x = 0;
+            int[] c = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 }; //********************
+
+            Shuffle(c);
+
+            foreach (Button b in panel1.Controls)
+            {
+                if (x < c.Length)
+                {
+                    b.Image = (Image)pieces[c[x]];
+                    x++;
+                }
+            }
+        }
+
+        public ArrayList ReturnCroppedList(Bitmap ToBeCropped, int x, int y)
+        {
+            ArrayList img_pieces = new ArrayList();
+            int moveright = 0;
+            int movedown = 0;
+
+            for (int k = 0; k < 16; k++)
+            {
+                Bitmap piece = new Bitmap(x, y);
+
+                for (int i = 0; i < x; i++)
+                    for (int j = 0; j < y; j++)
+                        piece.SetPixel(i, j, ToBeCropped.GetPixel(i + moveright, j + movedown)); //* **********************
+                img_pieces.Add(piece);
+
+                moveright += 110;
+
+                if (moveright == 440) { moveright = 0; movedown += 110; }
+                if (movedown == 440) { break; }
+            }
+
+            return img_pieces;
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Filter = "Image Files (JPEG,GIF,BMP,PNG,JPG)|*.jpeg;*.bmp;*.png;*.jpg";
+
+
+            openFileDialog1.RestoreDirectory = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                ToBeResize = Image.FromFile(openFileDialog1.FileName);
+                pictureBox1.Image = Resize(ToBeResize);
+                AddImagesToButton(ReturnCroppedList(Resize(ToBeResize), 110, 110));
+
+            }
+        }
+
+        private Bitmap Resize(Image img)
+        {
+            int w = 440;   //size of picturebox1
+            int h = 440;   //size of picturebox1
+
+            //creat a new Bitmap the size of the new image
+            Bitmap bmp = new Bitmap(w, h);
+            //creat a new graphic from the bitmap
+            Graphics graphic = Graphics.FromImage((Image)bmp);
+            graphic.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            //draw the newly resized image
+            graphic.DrawImage(img, 0, 0, w, h);
+            //dispose and free up th resources
+            graphic.Dispose();
+            //return the image
+            return bmp;
+        }
+
+        private void StretchButton_Click(object sender, EventArgs e)
+        {
+            if (StretchButton.Text == "<<<")
+            {
+                StretchButton.Text = ">>>";
+                this.Size = new Size(513, 567);
+            }
+            else if (StretchButton.Text == ">>>")
+            {
+                StretchButton.Text = "<<<";
+                this.Size = new Size(965, 567);
+            }
+        }
+
+        private void ShuffleButton_Click(object sender, EventArgs e)
+        {
+            AddImagesToButton(ReturnCroppedList(Resize(ToBeResize), 110, 110));
+        }
+
+        private int[] Shuffle(int[] array)
+        {
+            Random r = new Random();
+
+            int start = r.Next(1, array.Length);
+
+            for (int i = 0; i < array.Length; i++)
+                for (int j = start; j > 0; j--)
+                {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            return array;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            (new About()).ShowDialog();
+        }
+
+
+    }
+}
